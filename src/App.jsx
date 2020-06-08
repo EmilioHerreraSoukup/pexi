27ef69be0f31f3b7dce58ea95d7cd397f4afafe4
Emilio Herrera <emilioherrerasoukup@gmail.com>
2020-06-08 21:40:28 +0200

    add tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import ReactGA from 'react-ga';
+import dotenv from 'dotenv';
 import { ThemeProvider } from 'emotion-theming';
 import { Game } from './components/Game';
 import './App.css';
 import { getRandomColor } from './utils/utils';
+
+dotenv.config();
+
+ReactGA.initialize(process.env.GA_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   const theme = {
